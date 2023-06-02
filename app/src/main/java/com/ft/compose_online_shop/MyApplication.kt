433fe5b852e67464a62d3c5e8c944a4c0112a942
Fahad802 <fahad@hishabee.io>
2023-06-02 18:40:06 +0600

    update

--- app/src/main/java/com/ft/compose_online_shop/MyApplication.kt
+++ app/src/main/java/com/ft/compose_online_shop/MyApplication.kt
@@ -5,7 +5,6 @@ import android.app.Application
 import com.airbnb.lottie.compose.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import kotlin.text.Typography.dagger
 
 @HiltAndroidApp
 class MyApplication : Application() {
