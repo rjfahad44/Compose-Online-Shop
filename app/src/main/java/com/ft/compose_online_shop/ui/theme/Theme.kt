72e9f614aee0fdc9f79403d95795bd721d598019
Fahad802 <fahad@hishabee.io>
2023-06-03 10:53:21 +0600

    update

--- app/src/main/java/com/ft/compose_online_shop/ui/theme/Theme.kt
+++ app/src/main/java/com/ft/compose_online_shop/ui/theme/Theme.kt
@@ -1,26 +1,15 @@
 package com.ft.compose_online_shop.ui.theme
 
-import android.app.Activity
-import android.os.Build
+
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColors(
     primary = Blue,
@@ -55,7 +44,7 @@ fun ComposeOnlineShopTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: 
         LightColorPalette
     }
 
-    androidx.compose.material.MaterialTheme(
+    MaterialTheme(
         colors = colors,
         typography = Typography,
         shapes = Shapes,
